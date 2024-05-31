1ca09ccad2a18e0ec82cf3faad18c88b3c8cbd4d
Agneepradeep Verma <69806977+agneepradeep@users.noreply.github.com>
2024-05-31 23:40:40 +0530

    Delete JavaScript Lecture Notes/LECTURE 4/lecture4practice_1.js

--- JavaScript Lecture Notes/LECTURE 4/lecture4practice_1.js
+++
@@ -1,9 +0,0 @@
-//For a given array with marks of students -> [85,97,44,37,76,60]. Find the average marks of the entire class
-
-let marks = [85,97,44,37,76,60];
-let sum = 0;
-for(let i = 0;i<marks.length;i++){
-    sum += marks[i];
-}
-let average = sum/marks.length;
-console.log(`average marks of the class : ${average}`)
