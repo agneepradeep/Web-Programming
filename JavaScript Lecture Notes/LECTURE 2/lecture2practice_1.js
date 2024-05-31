e30e0fa4e79c5f08b14ac63048c8dffefd334e4d
Agneepradeep Verma <69806977+agneepradeep@users.noreply.github.com>
2024-05-31 20:06:40 +0530

    Delete JavaScript Lecture Notes/LECTURE 2/lecture2practice_1.js

--- JavaScript Lecture Notes/LECTURE 2/lecture2practice_1.js
+++
@@ -1,9 +0,0 @@
-/**Qs1. Get user to input a number using prompt("Enter a number: "). Check if the number is a multiple of 5 or not */
-
-let n = prompt("Enter a number:");
-
-if(n%5 == 0){
-    alert(n+" is a multiple of 5");
-} else {
-    alert(n+" is not a multiple of 5")
-}
